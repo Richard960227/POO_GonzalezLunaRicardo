4331e4c31d5dab39a824d9dc1775db146dc36c7d
Richard960227 <r.gonzalez.l@edu.utc.mx>
2021-11-18 17:00:50 -0600

    POO

---
+++ 08_Cadenas/Principal.java
@@ -0,0 +1,18 @@
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Cadena funciones = new Cadena();
+
+        funciones.cadenas();
+
+        masfuncionesCadena masfunciones = new masfuncionesCadena();
+
+        masfunciones.masFunciones();
+
+        cadenaInvertida inversion = new cadenaInvertida();
+
+        inversion.cadenaInvertidapalabras();
+
+    }
+}
