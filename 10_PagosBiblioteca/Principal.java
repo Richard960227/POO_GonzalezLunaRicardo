65d136df6ad9f945b13b209894ed7857bf1ae7a4
Richard960227 <r.gonzalez.l@edu.utc.mx>
2021-11-23 11:02:27 -0600

    POO

---
+++ 10_PagosBiblioteca/Principal.java
@@ -0,0 +1,87 @@
+/* 
+ *
+ * Vamos a realizar un programa mediante el cual el usuario pueda gestionar
+ * los pagos de los libros atrasados de la biblioteca
+ * 
+ */
+
+
+//libreria
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+    int choice;
+    String text_menu;
+
+    public static void main(String[] args) {
+
+        String text;
+        int opciones = 0;
+
+        /*
+         * Vamos a visualizar en una interfaz pequeña las opciones del menu
+         */
+
+        // menu
+        // instancia
+        Principal mainp = new Principal();
+
+        // ciclo para que el menu se repita y sea visible para el USUARIO
+        do {
+            mainp.MenuP();
+            text = JOptionPane.showInputDialog("En caso de que desee regresar al menu principal digite 1");
+            opciones = Integer.parseInt(text);
+
+        } while (opciones == 1);
+
+    }
+
+    public void MenuP() {
+
+        String text;
+        int opciones = 0;
+
+        // los metodos void (vacios), son porque no reciben ningun parametro
+
+        // excepciones
+
+        try {
+            // es para ingresar texto
+            text = JOptionPane.showInputDialog(
+                    "Selecciones el programa a ejecutar de la siguiente lista:" + "\n 1.-Consultar el Sueldo Personal."
+                            + "\n 2.-Costos y Proveedores." + "\n 3.- Prestamo y Devolucion de libros");
+            // cuando se utiliza JOptionPane solo se reciben cadenas
+            // voy a convertir esa cadena en un entero
+            opciones = Integer.parseInt(text);
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,
+                    "Para acceder a un Programa solo se puede ingresar los numeros del 1 al 3");
+            // vamos a obtener el error
+            JOptionPane.showMessageDialog(null, "" + e.getMessage());
+            // si solo queremos imprimir el error en consola
+            System.out.println("El error es: " + e.getMessage());
+        }
+
+        switch (opciones) {
+        case 1:
+            // metodo
+            break;
+
+        case 2:
+            // metodo
+            break;
+
+        case 3:
+            // metodo
+            break;
+
+        default:
+            JOptionPane.showMessageDialog(null, "Vuelva Pronto");
+            break;
+        }
+    }
+}
---
+++ 10_PagosBiblioteca/SueldoEmpleado.java
@@ -0,0 +1,8 @@
+/*
+ * Obtener los pagos correspondientes a los trabajdadores
+ * 
+ * Ingresar el nombre del trabajador
+ * sueldo
+ * tipo de trabajador
+ * 
+ */
