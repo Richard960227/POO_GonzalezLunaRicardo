62033ba41b3b6f73ea6bab85e221b517878c6ee5
Richard960227 <r.gonzalez.l@edu.utc.mx>
2021-12-02 20:25:01 -0600

    NetBeans

---
+++ 12_ArrayPersona/src/pkg12_arraypersona/Persona.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg12_arraypersona;
+
+/**
+ *
+ * @author dears
+ */
+public class Persona {
+    //atributos
+    private int id;
+    private String nombre;
+    private int edad;
+    //constructor
+    public Persona(){
+        
+    }
+
+    public Persona(int id, String nombre, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+}
