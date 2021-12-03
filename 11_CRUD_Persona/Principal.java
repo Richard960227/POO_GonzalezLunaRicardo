cba3a6b0de146d9774e3e8df84e4da199a7acfff
Richard960227 <r.gonzalez.l@edu.utc.mx>
2021-12-02 20:25:53 -0600

    POO

---
+++ 11_CRUD_Persona/Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        // aqui debemos de hacer la instancia para mandar a llamar al programa
+        ManipularPrograma obj = new ManipularPrograma();
+        obj.menu();
+    }
+}
