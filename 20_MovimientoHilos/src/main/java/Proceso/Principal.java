de7445268d162edf7bbbeeb07ee5ee32ac8b72b5
Richard960227 <r.gonzalez.l@edu.utc.mx>
2022-01-26 09:51:41 -0600

    NetBeans

---
+++ 20_MovimientoHilos/src/main/java/Proceso/Principal.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proceso;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author dears
+ */
+public class Principal extends JFrame {
+
+    public Principal() {
+        setTitle("Juego");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(900, 550);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        add(new Tablero());
+        setVisible(true);
+    }
+
+    public static void main(String args[]) {
+        new Principal();
+    }
+}
