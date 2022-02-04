7e629c2ab9a8e67fcefba4be9cba4346f9f48e75
Richard960227 <r.gonzalez.l@edu.utc.mx>
2022-02-04 14:09:52 -0600

    HTML

---
+++ 23.1_Formulario/js/validarformulario.js
@@ -0,0 +1,60 @@
+function validar(formulario){
+    //quiero validar el tamaño del nombre
+    if(formulario.nombre.value.length < 5){
+        alert("Escriba por lo menos 6 caracteres en el campo nombre");
+        //colocar cursor en el campo de error
+        formulario.nombre.focus();
+        return(false);
+    }
+
+    var checkOk = "ABCDEFGHIJKLMNOPQRSTUVW" + "abcdefghijklmnopqrstuvwxyz";
+    var checkStr = formulario.nombre.value;
+    var allvalid = true;
+    
+    for(var i=0; i<checkStr.length; i++){
+        var ch = checkStr.charAt(i);
+
+        for(var j=0; j<checkOk.length; j++){
+            if(ch == checkOk.charAt(j))
+                break;
+        }
+            if(j == checkOk.length){
+                allvalid = false;
+                break;
+            }
+    }
+    if(!allvalid){
+        alert("Escribe solo letras en el campo Nombre");
+        formulario.telefono.focus();
+        return (false);
+    }
+
+    if(formulario.telefono.value.length < 10){
+        alert("Escriba su Número a 10 digitos");
+        //colocar cursor en el campo de error
+        formulario.telefono.focus();
+        return(false);
+    }
+
+    var checkOk = "1234567890";
+    var checkStr = formulario.telefono.value;
+    var allvalid = true;
+    
+    for(var i=0; i<checkStr.length; i++){
+        var ch = checkStr.charAt(i);
+
+        for(var j=0; j<checkOk.length; j++){
+            if(ch == checkOk.charAt(j))
+                break;
+        }
+            if(j == checkOk.length){
+                allvalid = false;
+                break;
+            }
+    }
+    if(!allvalid){
+        alert("Escribe solo números en el campo Telefono");
+        formulario.telefono.focus();
+        return (false);
+    }
+}
