3bf8b553c5ea0fbf28f683dd04899c7c7ea03333
Richard960227 <r.gonzalez.l@edu.utc.mx>
2022-06-06 09:58:42 -0500

    HTML

--- 23_Formularios/js/validarformulario.js
+++ 23_Formularios/js/validarformulario.js
@@ -28,4 +28,44 @@ function validar(formulario){
         formulario.nombre.focus();
         return (false);
     }
+
+    if(formulario.edad.value.length < 10){
+        alert("Escriba su Edad a 2 digitos");
+        //colocar cursor en el campo de error
+        formulario.edad.focus();
+        return(false);
+    }
+
+    var checkOk = "1234567890";
+    var checkStr = formulario.edad.value;
+    var allvalid = true;
+    
+    for(var i=0; i<checkStr.length; i++){
+        var ch = checkStr.charAt(i);
+
+        for(var j=0; j<checkOk.length; j++){
+            if(ch == checkOk.charAt(j))
+                break;
+        }
+            if(j == checkOk.length){
+                allvalid = false;
+                break;
+            }
+    }
+    if(!allvalid){
+        alert("Escribe solo números en el campo Edad");
+        formulario.edad.focus();
+        return (false);
+    }
+
+    //manejo de expresiones regulares
+
+    var txt = formulario.correo.value;
+
+    var b = /^[^@\s]+@[^@\.\s]+(\.[^@\.\s]+)$)/;
+
+    alert("Email " + (b.test(txt) ? "" : "no ") + "valido");
+
+    return b.test(txt);
+
 }
