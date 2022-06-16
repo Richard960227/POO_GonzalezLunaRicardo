0079cfd493ea9239e52e184b8cef5ed5038c7627
Richard960227 <r.gonzalez.l@edu.utc.mx>
2022-06-16 12:06:02 -0500

    C++

--- C++/Examen/NumPrimo.cpp
+++ C++/Examen/NumPrimo.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
+
 using namespace std;
 
 int main() {
+	
+	cout<<"  **  NUMERO PRIMO  **\n\n";
 	int divisor = 1, divisores = 0, num = 0;
 	cout<<"Ingrese numero: ";
 	cin>>num;
