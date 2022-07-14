803067dfa15d8d5687bc82fb15b2981257a7024f
Richard960227 <r.gonzalez.l@edu.utc.mx>
2022-07-14 07:49:59 -0500

    C++

--- C++/06Cola/Tarjetahabientes.cpp
+++ C++/06Cola/Tarjetahabientes.cpp
@@ -3,12 +3,10 @@
 
 using namespace std;
 
-//definir el nodo
-
 struct nodo{
 	string dato;
-	int cuenta;
-	int cantidad;
+	int prioridad;
+	int deposito;
 	struct nodo *siguiente;
 };
 
@@ -21,14 +19,14 @@ struct cola{
 struct nodo *crearNodo(string x, int pri, int depo){
 	struct nodo *nuevoNodo = new(struct nodo);
 	nuevoNodo->dato = x;
-	nuevoNodo->cuenta = pri;
-	nuevoNodo->cantidad= depo;
+	nuevoNodo->prioridad = pri;
+	nuevoNodo->deposito= depo;
 	return nuevoNodo;
 }
 
-void encolar(struct cola &q, string valor, int cuenta, int cantidad){
+void encolar(struct cola &q, string valor, int priori, int deposito){
 	//auxiliar para mover el lado
-	struct nodo *aux = crearNodo(valor, cuenta, cantidad);
+	struct nodo *aux = crearNodo(valor, priori, deposito);
 	aux->siguiente = NULL;
 	
 	if(q.delante == NULL){
@@ -43,7 +41,7 @@ void encolar(struct cola &q, string valor, int cuenta, int cantidad){
 
 //mostrar la cola
 void mostrarCola(struct cola q){
-	nodo *aux = new nodo();
+	struct nodo *aux;
 	aux = q.delante;
 	
 	if(aux!=NULL){
@@ -51,7 +49,7 @@ void mostrarCola(struct cola q){
 		cout<<"\n\tTipo de Cuenta... \n\t1)VIP\n\t2)Empresarial\n\t3)Normal\n\t4)Sin Cuenta";
 		cout<<"\n\t|Tarjetahabiente"<<"|Tipo de Cuenta"<<"|Deposito"<<endl;
 		while(aux!=NULL){
-			cout<<"\t|"<<aux->dato<<"           |"<<aux->cuenta<<"             | $"<<aux->cantidad<<endl;
+			cout<<"\t|"<<aux->dato<<"           |"<<aux->prioridad<<"             | $"<<aux->deposito<<endl;
 			aux = aux->siguiente;
 		}
 	}else{
@@ -66,7 +64,8 @@ void ordenarPrioridad(struct cola &q){
 	//metodo de burbuja
 	
 	//necesito dos aux
-	struct nodo *aux1, *aux2;
+	struct nodo *aux1, *aux2, *aux3;
+	int d_aux;
 	int p_aux;
 	string c_aux;
 	
@@ -79,15 +78,27 @@ void ordenarPrioridad(struct cola &q){
 		
 		while(aux2 != NULL){
 			
-			if(aux1->cuenta > aux2->cuenta){
-				p_aux = aux1->cuenta;
+			aux3 = aux2->siguiente;
+			
+			while(aux3 != NULL){
+				if(aux1->prioridad > aux2->prioridad){
+				p_aux = aux1->prioridad;
 				c_aux = aux1->dato;
+				d_aux = aux1->deposito;
 				
-				aux1->cuenta = aux2->cuenta;
+				aux1->prioridad = aux2->prioridad;
 				aux1->dato = aux2->dato;
+				aux1->deposito = aux2->deposito;
 				
-				aux2->cuenta = p_aux;
+				aux2->prioridad = p_aux;
 				aux2->dato = c_aux;
+				aux2->deposito = d_aux;
+				
+				aux3->prioridad = p_aux;
+				aux3->dato = c_aux;
+				aux3->deposito = d_aux;
+				}
+				aux3 = aux3->siguiente;
 			}
 			aux2 = aux2->siguiente;
 		}
@@ -95,8 +106,8 @@ void ordenarPrioridad(struct cola &q){
 	}
 }
 
-void insertar(struct cola &q, string turno, int cuenta, int cantidad){
-	encolar(q,turno,cuenta,cantidad);
+void insertar(struct cola &q, string c, int cuenta, int pri){
+	encolar(q,c,cuenta,pri);
 	ordenarPrioridad(q);
 }
 
