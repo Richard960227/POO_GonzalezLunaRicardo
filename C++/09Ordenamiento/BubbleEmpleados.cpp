654938b6383d7061ef4aa940c8dc1263dc91d228
Richard960227 <r.gonzalez.l@edu.utc.mx>
2022-08-19 10:53:22 -0500

    C++

--- C++/09Ordenamiento/BubbleEmpleados.cpp
+++ C++/09Ordenamiento/BubbleEmpleados.cpp
@@ -1,67 +1,101 @@
-#include <stdio.h>
-#include <string.h>
+
+
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+
+#define MAX 50
+
+struct Empleado{
+	int codigo;
+	char nombre[20];
+	char apellido[20];
+}emp[100];
+
+struct ordenamiento{
+	int dia, mes, elem;
+};
+
+using namespace std;
+
+//metodos
+
+void leerarray(int, struct ordenamiento[]);
+void burbuja(int, struct ordenamiento[]);
+void mostrar(int, struct ordenamiento[]);
 
 int main(){
-	int i,N,j;
-	scanf("%d", &N);
-	char fechas[N][11],fechas_Mod[N][11];
-	int cont;
-	for(i=0;i<N;i++){
-		scanf("%s",fechas[i]);
-		//Formato en fechas_Mod (aaaa/mm/dd)
-		cont=0;
-		for(j=6;j<10;j++){ //Guardo aaaa
-			fechas_Mod[i][cont]=fechas[i][j];
-			cont++;
-		}
-		fechas_Mod[i][cont]='/';cont++;
-		for(j=3;j<5;j++){ //Guardo mm
-			fechas_Mod[i][cont]=fechas[i][j];
-			cont++;
-		}
-		fechas_Mod[i][cont]='/';cont++;
-		for(j=0;j<2;j++){ //Guardo dd
-			fechas_Mod[i][cont]=fechas[i][j];
-			cont++;
-		}
-		fechas_Mod[i][cont]='\0';
-	}
+	int n;
+	struct ordenamiento orden[MAX];
 	
-	//Obtener los vectores de punteros
-	char *vec_Fechas[N], *vec_Fechas_Mod[N];
-	for(i=0;i<N;i++){
-		vec_Fechas[i]=fechas[i];
-		vec_Fechas_Mod[i]=fechas_Mod[i];
-	}
+	cout<<"Ingresa el limite del arreglo: ";
+	cin>>n;
 	
-	//Proceso ORdenamiento
-	int continuar;
-	char *aux;
-	i=0;
-	do{
-		continuar=0;
-		for(j=0;j<(N-i-1);j++){
-			if(strcmp(vec_Fechas_Mod[j],vec_Fechas_Mod[j+1])>0){
-				//Intercambiar vec_fechas_Mod[i] y vec_fechas_Mod[i+1]
-				aux=vec_Fechas[i];
-				vec_Fechas[j] = vec_Fechas[j+1];
-				vec_Fechas[j+1]=aux;
-				
-				aux=vec_Fechas_Mod[j];
-				vec_Fechas_Mod[j] = vec_Fechas[j+1];
-				vec_Fechas_Mod[j+1]=aux;
-				
-				continuar=1;
+	leerarray(n, orden);
+	burbuja(n, orden);
+	
+	cout<<"\n El arreglo ordenado es: \n";
+	
+	mostrar(n, orden);
+	
+	getch();
+}
+
+void leerarray(int n, struct ordenamiento a[]){
+	
+	
+	//ingresar los valores del arreglo
+	for(int i=0; i<n; i++){
+		fflush(stdin);
+		cout<<"Nombre: ";
+		cin.getline(emp[i].nombre,20,'\n');
+		cout<<"Apellido: ";
+		cin.getline(emp[i].apellido,20,'\n');
+		cout<<"Fecha de Nacimiento";
+		cout<<endl<<"Dia de Nacimiento: ";
+		cin>>a[i].dia;
+		cout<<"Mes de Nacimiento: ";
+		cin>>a[i].mes;
+		cout<<"Año de Nacimiento: ";
+		cin>>a[i].elem;
+		cout<<endl<<"Codigo de Empleado: ";
+		cin>>emp[i].codigo;
+		cout<<"\n";
+	}
+}
+
+void burbuja(int n, struct ordenamiento a[]){
+	int i, j;
+	//aqui guardo los datos ordenados
+	struct ordenamiento temp;
+	
+	//aplico burbuja
+	for(i=1; i<n; i++){
+		for(j=n-1; j>=i; j--){
+			//realizo la comparacion
+			if(a[j-1].elem>a[j].elem){
+				//aqui hacemos el cambio de posiciones
+				temp = a[j-1];
+				a[j-1] = a[j];
+				a[j] = temp;
 			}
 		}
-		i++;
-	}while(continuar);
-	
-	//Imprimir Resultados
-	for(i=0;i<N;i++){
-		printf("%s\n", vec_Fechas[i]);
 	}
-	
-	return 0;
-	
+}
+
+void mostrar(int n, struct ordenamiento a[]){
+	//imprimir elemento por elemento
+	for(int i = 0; i<n; i++){
+		cout<<"__________________________________"<<endl;
+		cout<<"|"<<endl;
+		cout<<"| Nombre: "<<emp[i].nombre<<endl;
+		cout<<"| Apellido: "<<emp[i].apellido<<endl;
+		cout<<"| Fecha de Nacimiento:";
+		cout<<a[i].dia<<"/";
+		cout<<a[i].mes<<"/";
+		cout<<a[i].elem<<endl;
+		cout<<"| Codigo: "<<emp[i].codigo<<endl;
+		cout<<"|_________________________________";
+		cout<<endl;
+	}
 }
